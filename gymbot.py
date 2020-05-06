5e60076922af0b3829867b792fdfdd363ca9362a
alec <alec@implicitdesign.co.za>
2020-05-06 20:49:01 +0200

    added gifs

--- gymbot.py
+++ gymbot.py
@@ -6,31 +6,31 @@ import os
 
 exercises = {
     "upper": {
-        "Push Ups": [20, "reps"],
-        "Burpees": [20, "reps"],
-        "Shoulder Taps": [20, "reps"],
-        "Inch Worms": [10, "reps"],
-        "Bear Crawls": [30, "seconds"],
-        "Tricep Dips": [20, "reps"],
-        "Close-Grip Push Ups": [10, "reps"],
-        "Kick Throughs": [20, "reps"],
+        "Push Ups": [20, "reps", "https://media.giphy.com/media/Kjj5yTgDdiuvC/giphy.gif"],
+        "Burpees": [20, "reps", "https://media.giphy.com/media/23hPPMRgPxbNBlPQe3/giphy.gif"],
+        "Shoulder Taps": [20, "reps", "https://cdn.shopify.com/s/files/1/2930/0432/files/shoulder_taps_large.gif?v=1540063651"],
+        "Inch Worms": [10, "reps", "https://media.giphy.com/media/UTXzXAwUHGx8MDEtPS/giphy.gif"],
+        "Bear Crawls": [30, "seconds", "https://media.giphy.com/media/LWBkgGLVvDBJK/giphy.gif"],
+        "Tricep Dips": [20, "reps", "https://media.giphy.com/media/13HOBYXe87LjvW/giphy.gif"],
+        "Close-Grip Push Ups": [10, "reps", "http://www.shapefit.com/pics/chest-exercises-push-ups-close-hand-position.gif"],
+        "Kick Throughs": [20, "reps", ""],
     },
     "legs": {
-        "Squats": [20, "reps"],
-        "Lunges": [20, "reps"],
-        "High Knees": [30, "seconds"],
-        "Mountain Climbers": [30, "seconds"],
-        "Squat Jumps": [20, "reps"],
-        "Side Lunges": [20, "reps"],
-        "Reverse Lunges": [20, "reps"],
+        "Squats": [20, "reps", "https://media.giphy.com/media/1qfKN8Dt0CRdCRxz9q/giphy.gif"],
+        "Lunges": [20, "reps", "https://media.giphy.com/media/l3q2Q3sUEkEyDvfPO/giphy.gif"],
+        "High Knees": [30, "seconds", "https://media.giphy.com/media/l0HlNOsSRC0Bts7iU/giphy.gif"],
+        "Mountain Climbers": [30, "seconds", "https://media.giphy.com/media/bWYc47O3jSef6/giphy.gif"],
+        "Squat Jumps": [20, "reps", "https://media.giphy.com/media/nmuUOAEvrKTLDT3yTn/giphy.gif"],
+        "Side Lunges": [20, "reps", "https://media.giphy.com/media/Pj0wnhvHp3AHMM5ILf/giphy.gif"],
+        "Reverse Lunges": [20, "reps", "https://media.giphy.com/media/3o6ozoyJ0IlfuEsuXu/giphy.gif"],
     },
     "core": {
-        "Sit Ups": [20, "reps"],
-        "Planks": [30, "seconds"],
-        "Side Planks": [30, "seconds"],
-        "Supermans": [20, "reps"],
-        "Leg Raises": [20, "reps"],
-        "Bicycle Crunches": [20, "reps"],
+        "Sit Ups": [20, "reps", "https://media.giphy.com/media/9EFCRjJF4EqB2/giphy.gif"],
+        "Planks": [30, "seconds", "https://media.giphy.com/media/xT8qBff8cRRFf7k2u4/giphy.gif"],
+        "Side Planks": [30, "seconds", "https://media.giphy.com/media/3o6gDUTsbepOYTqTRK/giphy.gif"],
+        "Supermans": [20, "reps", "https://media.giphy.com/media/PmPRDFY1ENYMo/giphy.gif"],
+        "Leg Raises": [20, "reps", "https://media.giphy.com/media/2LtUR24UvCZdC/giphy.gif"],
+        "Bicycle Crunches": [20, "reps", "https://media.giphy.com/media/TMNCtgJGJnV8k/giphy.gif"],
     },
 }
 
@@ -65,37 +65,39 @@ upper_val = exercises["upper"][upper_key]
 legs_val = exercises["legs"][legs_key]
 core_val = exercises["core"][core_key]
 
-text = ""
+pretext = ""
 
 if datetime.datetime.now().hour == 8:
-    text += "Good morning sunshine! \n Now "
+    pretext += "Good morning sunshine! \n Now "
 elif datetime.datetime.now().hour == 10:
-    text += "Just before you have that cookie... "
+    pretext += "Just before you have that cookie... "
 elif datetime.datetime.now().hour == 12:
-    text += "Lunch is going to taste so much better if you "
+    pretext += "Lunch is going to taste so much better if you "
 elif datetime.datetime.now().hour == 13:
-    text += "Belly full or barely full, it doesn't change the fact you need to "
+    pretext += "Belly full or barely full, it doesn't change the fact you need to "
 elif datetime.datetime.now().hour == 16:
-    text += "You're almost there big boy... \n Now "
+    pretext += "You're almost there big boy... \n Now "
 elif datetime.datetime.now().hour == 17:
-    text += "If you've made it this far, you might as well go all the way... "
+    pretext += "If you've made it this far, you might as well go all the way... "
 else:
-    text += "Just "
+    pretext += "Just "
 
-text += "drop and give me: \n *Upper body* \n {} ({} {}) \n *Legs* \n {} ({} {}) \n *Core* \n {} ({} {}) \n".format(
-    upper_key,
-    upper_val[0],
-    upper_val[1],
-    legs_key,
-    legs_val[0],
-    legs_val[1],
-    core_key,
-    core_val[0],
-    core_val[1],
-)
+pretext += "drop and give me: \n "
+
+# text += "*Upper body* \n {} ({} {}) \n *Legs* \n {} ({} {}) \n *Core* \n {} ({} {}) \n".format(
+#     upper_key,
+#     upper_val[0],
+#     upper_val[1],
+#     legs_key,
+#     legs_val[0],
+#     legs_val[1],
+#     core_key,
+#     core_val[0],
+#     core_val[1],
+# )
 
 icon_emoji = "aw_yeah"
-channel = "#workout"
+channel = "#test"# "#workout"
 username = "GymBot"
 url = os.environ.get("SLACK_WEBHOOK_URL")
 
@@ -105,9 +107,55 @@ else:
     requests.post(
         url,
         json={
-            "text": text,
-            "icon_emoji": icon_emoji,
-            "channel": channel,
-            "username": username,
-        },
+            'text': pretext,
+            'unfurl_links': True,
+            "unfurl_media": True,
+            'icon_emoji': icon_emoji,
+            'channel': channel,
+            'username': username,
+            'attachments': [
+                {
+                    "fallback": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "pretext": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "color":"#FAA71A",
+                    "image_url": upper_val[2],
+                    "fields": [
+                        {
+                            "title": "Upper body",
+                            "value": "{} ({} {})".format(upper_key, upper_val[0], upper_val[1]),
+                            "image_url": upper_val[2],
+                            "short": False
+                        }
+                    ]
+                },
+                {
+                    "fallback": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "pretext": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "color":"#FAA71A",
+                    "image_url": legs_val[2],
+                    "fields": [
+                        {
+                            "title": "Legs",
+                            "value": "{} ({} {})".format(legs_key, legs_val[0], legs_val[1]),
+                            "image_url": legs_val[2],
+                            "short": False
+                        }
+                    ]
+                },
+                {
+                    "fallback": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "pretext": "", # "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                    "color":"#FAA71A",
+                    "image_url": core_val[2],
+                    "fields": [
+                        {
+                            "title": "Core",
+                            "value": "{} ({} {})".format(core_key, core_val[0], core_val[1]),
+                            "image_url": core_val[2],
+                            "short": False
+                        }
+                    ]
+                }
+            ]
+        }
     )
